51daea8eaeb119efd1f4ea8f92a014a070c0ab89
lonercode <penpalclassmate@gmail.com>
2023-05-09 12:48:39 -0700

    yas file

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    replace = ""
-    str = replace + str[:n] + str[n + 1:]
-    return str
+    if n > 0:
+        replace = ""
+        str = replace + str[:n] + str[n + 1:]
+        return str
+    else:
+        return str
