806946cea1cc33acf497a5ce4cafcbca5c4c9aa8
lonercode <penpalclassmate@gmail.com>
2023-05-09 06:46:54 -0700

    file

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i == 99:
-        print(f"{i:02}", end = "")
+for num in range(0, 100):
+    if num == 99:
+        print("{}".format(num:02), end="")
     else:
-        print(f"{i:02}", end = ",")
+        print("{}".format(num:02), end=",")
