b84689af82923abcd02a5d5c0f6fec4370d93173
lonercode <penpalclassmate@gmail.com>
2023-05-09 11:33:14 -0700

    filez

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 for i in range(122, 96, -1):
     if i % 2 != 0:
-        print("{}".format(chr(i - 32)), end="")
+        j = 32
     else:
-        print(chr(i), end="")
+        j = 0
+    print("{}".format(chr(i - j)), end="")
