7e2772ba2b30b79ed6e8d7e2e99ff50df4bd144e
lonercode <penpalclassmate@gmail.com>
2023-05-11 13:50:00 -0700

    last correction hopefully

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    
+
     operatorD = {"+": add, "-": sub, "*": mul, "/": div}
     operator = sys.argv[2]
     if operator not in list(operatorD.keys()):
