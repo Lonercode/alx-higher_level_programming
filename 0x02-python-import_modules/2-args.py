8bc9c2128b3f58a86289f8348227c0440ee9f505
lonercode <penpalclassmate@gmail.com>
2023-05-11 04:59:58 -0700

    filez

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,11 +4,13 @@ if __name__ == "__main__":
     import sys
 
     argNum = len(sys.argv)
-    if argNum < 2:
+    if argNum < 2 and argNum > 0:
         print("0 arguments.".format(argNum-1))
 
     if argNum == 2:
         print("{} argument.".format(argNum-1))
+        for i in range(2, argNum + 1):
+            print("{}: {}".format(i-1, sys.argv[i-1]))
 
     if argNum > 3:
         print("{} arguments:".format(argNum-1))
