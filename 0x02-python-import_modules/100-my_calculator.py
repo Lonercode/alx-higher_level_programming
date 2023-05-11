93215685457ef99ef1c924e12f0d76a51ae337b6
lonercode <penpalclassmate@gmail.com>
2023-05-11 06:06:08 -0700

    mycalc

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
+
+
+    if len(sys.argv) == 4:
+        if operator == '+':
+            result = add(a, b)
+            print("{} {} {} = {}".format(a, operator, b, result))
+        elif operator == '-':
+            result = sub(a, b)
+            print("{} {} {} = {}".format(a, operator, b, result))
+        elif operator == '*':
+            result = mul(a, b)
+            print("{} {} {} = {}".format(a, operator, b, result))
+        elif operator == '/':
+            result = div(a, b)
+            print("{} {} {} = {}".format(a, operator, b, result))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+
+    else:
+        print("Usage: {} {} {} {}".format(sys.argv[0], a, operator, b))
+        sys.exit(1)
