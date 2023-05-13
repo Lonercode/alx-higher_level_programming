8004f6651d7e93c5fb2ec0f259256ddbc2206b40
lonercode <penpalclassmate@gmail.com>
2023-05-13 14:31:17 -0700

    filez

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def new_in_list(my_list, idx, element):
-        if idx < 0:
-            return (my_list)
-        if idx > len(my_list) - 1:
-            return (my_list)
-        if idx > 0 and idx < len(my_list):
-            my_list1 = []
-            for i in my_list:
-                my_list1.append(i)
-            my_list1[idx] = element
-            return (my_list1)
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx > len(my_list) - 1:
+        return (my_list)
+    else:
+        my_list1 = []
+        for i in my_list:
+            my_list1.append(i)
+        my_list1[idx] = element
+        return (my_list1)
