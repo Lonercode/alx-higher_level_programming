953a56bd7548fa41ac876361735b0e7e918ed85e
lonercode <penpalclassmate@gmail.com>
2023-05-13 14:41:55 -0700

    filez

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def multiple_returns(sentence):
-        if sentence == "":
-            first_char = None
-        else:
-            first_char = sentence[0]
+def multiple_returns(sentence):
+    if sentence == "":
+        first_char = None
+    else:
+        first_char = sentence[0]
 
-        tup_list = []
-        length = len(sentence)
+    tup_list = []
+    length = len(sentence)
 
-        tup_list.append(length)
-        tup_list.append(first_char)
+    tup_list.append(length)
+    tup_list.append(first_char)
 
-        new_tuple = tuple(tup_list)
-        return (new_tuple)
+    new_tuple = tuple(tup_list)
+    return (new_tuple)
