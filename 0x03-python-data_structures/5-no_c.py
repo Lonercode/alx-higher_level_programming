69e34ce06147c0995835c9d26c4e72a74fe7b8a4
lonercode <penpalclassmate@gmail.com>
2023-05-13 14:33:30 -0700

    filez

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def no_c(my_string):
-        new_string = []
-        for letter in my_string:
-            if letter == 'c' or letter == 'C':
-                letter = ""
-            new_string.append(letter)
-        final_string = ''.join(str(letter) for letter in new_string)
-        return (final_string)
+def no_c(my_string):
+    new_string = []
+    for letter in my_string:
+        if letter == 'c' or letter == 'C':
+            letter = ""
+        new_string.append(letter)
+    final_string = ''.join(str(letter) for letter in new_string)
+    return (final_string)
