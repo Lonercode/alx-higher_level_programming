c5ccd16a54b911883195e3ca0a1d8ebafdf9eca8
lonercode <penpalclassmate@gmail.com>
2023-05-13 14:09:32 -0700

    filez

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
     def element_at(my_list, idx):
-        if idx < 0:
+        if idx < 0 or idx > len(my_list) - 1:
             return (None)
         if idx > len(my_list) - 1:
             return (None)
