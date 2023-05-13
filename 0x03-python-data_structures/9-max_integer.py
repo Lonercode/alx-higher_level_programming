9831619575530998307c71b077d611c2973fe3b2
lonercode <penpalclassmate@gmail.com>
2023-05-13 14:46:41 -0700

    filez

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def max_integer(my_list=[]):
-        if my_list == []:
-            return (None)
-        else:
-            max = 0
-            for integer in range(0, len(my_list)):
-                if my_list[integer] > max:
-                    max = my_list[integer]
-            return (max)
+def max_integer(my_list=[]):
+    if my_list == []:
+        return (None)
+    else:
+        max = 0
+        for integer in range(0, len(my_list)):
+            if my_list[integer] > max:
+                max = my_list[integer]
+        return (max)
