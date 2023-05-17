5bcaba796beda0052826f00c07563a55e5694ab7
lonercode <penpalclassmate@gmail.com>
2023-05-17 10:43:50 -0700

    Added filez

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if len(a_dictionary) == 0 or not isinstance(a_dictionary, dict):
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return (None)
     else:
         max = list(a_dictionary.values())[0]
