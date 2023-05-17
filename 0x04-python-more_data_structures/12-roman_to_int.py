d891f9463637a8789ad8c79b975126008f7eef44
lonercode <penpalclassmate@gmail.com>
2023-05-17 10:07:42 -0700

    Added filez

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if isinstance(roman_string, str) == False:
+    if isinstance(roman_string, str) is False:
         return (0)
     else:
-        dict1 = {'I': 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100}
+        dict1 = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100}
         val = 0
         for i in range(len(roman_string)):
             if i > 0 and dict1[roman_string[i]] > dict1[roman_string[i - 1]]:
