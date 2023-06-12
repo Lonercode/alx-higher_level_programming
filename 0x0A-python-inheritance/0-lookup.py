1b8d5ab634e9f29c5f691a447903403206b4ad88
lonercode <penpalclassmate@gmail.com>
2023-06-12 05:23:03 -0700

    files

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """
-Lookup
+module for lookup.
 """
 
+
 def lookup(obj):
-    """returns the list of available attributes and methods of an object"""
+    """Return the attrs"""
     return dir(obj)
