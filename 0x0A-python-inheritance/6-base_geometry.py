58c3a50fc2d5a097c80275db9ba33be66cd10960
lonercode <penpalclassmate@gmail.com>
2023-06-12 05:50:22 -0700

    files

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -7,7 +7,6 @@ BaseGeometry class with exception
 class BaseGeometry:
     """BaseGeometry class"""
 
-
     def area(self):
         """Exception"""
         raise Exception("area() is not implemented")
